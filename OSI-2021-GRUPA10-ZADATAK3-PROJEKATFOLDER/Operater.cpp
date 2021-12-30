edbdd5bcb86540c029da48315db12cdd4ef5f0ce
sinisa-radeta <sinisa.radeta@student.etf.unibl.org>
2021-12-30 18:41:02 +0100

    komitujem klasu Operater i njenu funkciju clanicu ispisSvihLetova

---
+++ OSI-2021-GRUPA10-ZADATAK3-PROJEKATFOLDER/Operater.cpp
@@ -0,0 +1,28 @@
+//
+// Created by sinisa on 12/30/21.
+//
+
+#include "Operater.h"
+namespace fs = std::filesystem;
+
+Operater::Operater(std::string username, std::string password)
+{
+    this->korisnickoIme = username;
+    this->lozinka = password;
+    this->radnoMjesto = "operater";
+
+}
+
+void Operater::ispisSvihLetova()
+{
+    namespace fs = std::filesystem;
+    std::string path = "\\LETOVI";
+    for (const auto &entry: fs::directory_iterator(path)) {
+
+        std::string string = entry.path();
+        string.erase(0,9);
+        string.erase(string.length()-4);
+
+        std::cout << string << std::endl;
+    }
+}
