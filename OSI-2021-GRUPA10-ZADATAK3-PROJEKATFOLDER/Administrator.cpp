e5501604f2f3b98374d94b033b5d3d859a3bca75
GordanInzenjer <gordonletic5@gmail.com>
2021-12-30 18:14:58 +0100

    Dodana metoda za kreiranje radnika u klasi Administrator-ZADATAK 3

--- OSI-2021-GRUPA10-ZADATAK3-PROJEKATFOLDER/Administrator.cpp
+++ OSI-2021-GRUPA10-ZADATAK3-PROJEKATFOLDER/Administrator.cpp
@@ -1,9 +1,42 @@
 #include "Administrator.h"
+#include <fstream>
+#include <string>
 
-bool Administrator::dodajRadnika(Radnik r){
+Administrator::Administrator(std::string korisnickoIme, std::string lozinka)
+{
+	Radnik (korisnickoIme, lozinka, "Administrator");
+}
 
+
+bool Administrator::dodajRadnika(){
+	auto pisiUFajl = std::ofstream("radnici.dat", std::ios::binary | std::ios::app);
+	if (pisiUFajl) {
+		std::string korisnickoIme, lozinka, radnoMjesto;
+		
+		std::cout << "Dodajete podatke o korisniku:" << std::endl;
+		std::cout << "(Napomena: Unositi tip radnog mjesta sa pocetnim velikim slovom!)" << std::endl;
+		std::cout << "Korisnicko ime: "; std::cin >> korisnickoIme;
+		std::cout << "Lozinka: "; std::cin >> lozinka;
+		do
+		{
+			std::cout << "Radno mjesto: "; std::cin >> radnoMjesto;
+
+		}while ((radnoMjesto != "Sef" && radnoMjesto != "Administrator" && radnoMjesto!= "Kontrolor"
+			&& radnoMjesto != "Operater"));
+		Radnik r(korisnickoIme,lozinka, radnoMjesto);
+		pisiUFajl.write((char*)&r, sizeof(Radnik));
+		pisiUFajl.close();
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 bool Administrator::suspendujRadnika(Radnik r) {
+	return true;
+}
 
+bool Administrator::obrisiRadnika(Radnik r) {
+	return true;
 }
