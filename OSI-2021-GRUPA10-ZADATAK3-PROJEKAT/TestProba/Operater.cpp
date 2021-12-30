bb248655eeec34d83151f45e92d537dc2c0c23e3
DESKTOP-SV2RG4H\Тепица <acivon@outlook.com>
2021-12-30 20:04:06 +0100

    Vracen naziv fajla - ZADATAK 3

--- OSI-2021-GRUPA10-ZADATAK3-PROJEKAT/TestProba/Operater.cpp
+++ OSI-2021-GRUPA10-ZADATAK3-PROJEKAT/TestProba/Operater.cpp
@@ -15,14 +15,20 @@ Operater::Operater(std::string username, std::string password)
 
 void Operater::ispisSvihLetova()
 {
-    namespace fs = std::filesystem;
+    /*namespace fs = std::filesystem;
     std::string path = "\\LETOVI";
     for (const auto& entry : fs::directory_iterator(path)) {
 
-        //std::string string = entry.path();
-        //string.erase(0, 9);
-        //string.erase(string.length() - 4);
+        std::string string = entry.path();
+        string.erase(0, 9);
+        string.erase(string.length() - 4);
 
-        //std::cout << string << std::endl;
-    }
+        std::cout << string << std::endl;
+    }*/
+
+    fs::path path = std::filesystem::current_path();
+
+    for (auto const& entry : fs::directory_iterator(path / "LETOVI"))
+        std::cout << entry.path().filename() << std::endl;
+
 }
